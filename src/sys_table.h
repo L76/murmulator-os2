d7d70d8f71dfc461462597cb6be65bc95e248376
Igor <identifier.L76@gmail.com>
2025-08-30 21:39:19 +0200

    compilation issue

--- src/sys_table.h
+++ src/sys_table.h
@@ -2,4 +2,4 @@
 ///#define __in_boota(group) __attribute__((section(".boota" group)))
 #define __in_hfa(group) __attribute__((section(".high_flash" group)))
 
-extern unsigned long __in_systable() __aligned(4096) sys_table_ptrs[];
+extern unsigned long __in_systable() __attribute__ ((aligned (4096))) sys_table_ptrs[];
